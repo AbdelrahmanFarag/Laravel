e7a227ddb02d5e55a0cad3a14137a1fbd01ec2d0
AbdelrahmanFarag <farajabdelrahman@gmail.com>
2020-09-13 11:08:29 +0200

    Add files via upload

---
+++ resources/views/partials/admin-header.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a id="a" class="navbar-brand" href="{{route('blog.index')}}">Products</a>
+
+        </div>
+    </div>
+</nav>
+
+
