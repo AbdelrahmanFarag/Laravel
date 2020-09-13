bcaed0fd113b0195a644158ce930b79753f9763b
AbdelrahmanFarag <farajabdelrahman@gmail.com>
2020-09-13 11:04:40 +0200

    Create .about.blade.php

---
+++ resources/views/other/.about.blade.php
@@ -0,0 +1,19 @@
+@extends("Layouts.master")
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">About me</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <p>
+                Our site provides many medical services at the same time as it contracts with reliable companies and owners of modern equipment
+                that the hospital needs. All you have to do is search for what you want and book it through our site, whether it is an unsupported
+                medicine or special medical devices, and we will bring it to your home or hospital In which you are.
+            </p>
+        </div>
+    </div>
+
+@endsection
